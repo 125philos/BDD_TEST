53737d439d2a1c4a13ef339db86e26da47db2dd7
Olessya <philos.dec@gmail.com>
2018-04-29 11:22:15 +0700

    Добавили методы в ConEncr.java.

--- ConEncr.java
+++ ConEncr.java
@@ -7,4 +7,15 @@ public class ConEncr {
 	{
 		
 	}
+	
+	public static final int SYMBOL = 0; //символ
+    
+    /**
+     * Получить зашифрованный коэффициент из массива по его индексу
+     * @param index наименование шифрованного значения, тип int
+     * @return коэффициент шифрования
+     */
+    public static double getValues(int index) {
+        return 110; 
+    }
 }
