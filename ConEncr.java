b3cb97f13e4c4d303470ec54daf910eaed408fbe
Olessya <philos.dec@gmail.com>
2018-04-29 12:18:05 +0700

    Добавили заглушку для получения двоичного кода зашифрованного символа.

--- ConEncr.java
+++ ConEncr.java
@@ -8,14 +8,15 @@ public class ConEncr {
 		
 	}
 	
-	public static final int SYMBOL = 0; //символ
-    
+	public static final int SYMBOL = 6; //символ
+	public static final int A = 4; //символ
     /**
      * Получить зашифрованный коэффициент из массива по его индексу
      * @param index наименование шифрованного значения, тип int
      * @return коэффициент шифрования
      */
     public static double getValues(int index) {
-        return 110; 
+        if(index == A) return 100;
+    	return 110;
     }
 }
--- shifr.feature
+++ shifr.feature
@@ -13,3 +13,5 @@ Feature: Software Encryptor
     When I have entered A as first operand
     And I press 'Get encrypt'
     Then The result should be 100
+
+
