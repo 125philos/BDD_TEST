305f7e8bc631b2d70d4e58bf21f8fd809dba80d3
Olessya <philos.dec@gmail.com>
2018-04-30 08:46:59 +0700

    Добавлена заглушка метода в классе ConEncr.java для всех преобразовывающихся символов.

--- ConEncr.java
+++ ConEncr.java
@@ -10,16 +10,37 @@ public class ConEncr {
 	
 	public static final int SYMBOL = 6; //символ
 	public static final int A = 4; //символ
-	public static final int B = 10; //символ
+	public static final int B = 5; //символ
+	public static final int C = 6; //символ
+	public static final int D = 7; //символ
+	public static final int E = 9; //символ
+	public static final int G = 10; //символ
+	public static final int J = 11; //символ
+	public static final int T = 12; //символ
 
-    /**
-     * Получить зашифрованный коэффициент из массива по его индексу
-     * @param index наименование шифрованного значения, тип int
-     * @return коэффициент шифрования
-     */
+	//Массив коэффициентов конвертирования
+	private static int[] shifrov = {
+			100, //A
+			101, //B
+			110, //C
+			111, //D
+			1001, //E
+			1010, //G
+			1011, //J
+			1100 //T
+	};
+
+	/**
+	 * Получаем зашифрованный символ из массива по его индексу
+	 * @param index индекс в массиве, тип int
+	 * @return зашифрованный символ, записанный по данному индексу, тип int
+	 * @throws return -1 в случае, если передается индекс, выходящий за пределы массива
+	 */
     public static double getValues(int index) {
-        if(index == A) return 100;
-        else if(index == B) return 1010;
-    	return 110;
+		if (index < 0 || index > shifrov.length)
+			return -1;
+		else
+			return shifrov[index];
+	}
     }
 }
