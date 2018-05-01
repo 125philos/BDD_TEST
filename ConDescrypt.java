a3ad6a19332d643b695d2519c12be175110c07ae
Olessya <philos.dec@gmail.com>
2018-05-01 14:51:06 +0700

    Добавляем заглушку в класс ConDescrypt.java для расшифровки символа A.

--- ConDescrypt.java
+++ ConDescrypt.java
@@ -6,4 +6,15 @@ public class ConDescrypt {
     public ConDescrypt() {
 
     }
+
+    public static final int A = 100; //символ
+
+    /**
+     * Получить расшифрованный коэффициент из массива по его индексу
+     * @param index наименование шифрованного значения, тип int
+     * @return коэффициент расшифрованого значения
+     */
+    public static double getValuesDes(int index) {
+        return 4;
+    }
 }
